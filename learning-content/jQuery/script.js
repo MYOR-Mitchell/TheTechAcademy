29d1a31867ecbd9920f2e2f34b2bdd17ab0d6d34
MYOR-Mitchell <Martin.Mitchell@MyYahoo.com>
2024-12-17 11:58:05 -0500

    more jQuery practice.

--- learning-content/jQuery/script.js
+++ learning-content/jQuery/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function(){ //This function will run when the document is finished loading.
 
     //The shineLoop function uses the chaining technique to utilize multiple animate 
     //event methods in the same function.
@@ -10,4 +10,37 @@ $(document).ready(function(){
     //setInterval will repeat the shineLoop function and 0 sets the delayed amount
     //of time between each repeat.
     setInterval(shineLoop, 0);
+
+    //Shrinks header size when the document is scrolled down by 50 pixels.
+    $(document).on("scroll", function() {
+        if ($(document).scrollTop() > 50) {
+            $("h1").addClass("header-scroll");
+        } else {
+            $("h1").removeClass("header-scroll");
+        }
+    });
+
+    //Triigers the images to slide down one after the other when the user's mouse
+    //enters the div area with the #classicCars ID.
+    $("#classicCars").on({
+        mouseenter: function() {
+            $("#titleOne").show(1000);
+            $("#titleTwo").show(1500);
+            $("#titleThree").show(2000);
+        },
+
+        //Triggers the images to slide up and hide each title one after the other
+        //when the user's mouse leaves the div area with the #classicCars ID.
+        mouseleave: function() {
+            $("#titleOne").hide(2000);
+            $("#titleTwo").hide(1500);
+            $("#titleThree").hide(1000);
+        }
+    });
+
+    //Creates a toggle efffect on each faq question by checking each element's sibling so they don't
+    //all display when one question is clicked
+    $('div.question').on('click', function() {
+        $(this).next().slideToggle('slow');
+    });
 })
