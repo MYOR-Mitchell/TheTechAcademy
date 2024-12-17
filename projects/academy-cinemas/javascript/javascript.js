4cbc67d1635d3763c19107efa79193baf0849d64
MYOR-Mitchell <Martin.Mitchell@MyYahoo.com>
2024-12-17 12:47:16 -0500

    Bootstrap / javascript
    popovers

---
+++ projects/academy-cinemas/javascript/javascript.js
@@ -0,0 +1,13 @@
+
+//initialize bootstrap popovers
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+
+popoverTriggerList.forEach(function (element) {
+    var imgSrc = element.getAttribute('data-bs-img');
+    var content = "<img class='star-rating' img src='" + imgSrc + "'>";
+    new bootstrap.Popover(element, {
+        content: content,
+        html: true,
+        trigger: 'hover',
+    });
+});
