fc40f691bf6fdea94c9cd660c08ec3a099fe3d17
MYOR-Mitchell <Martin.Mitchell@MyYahoo.com>
2024-12-17 22:46:39 -0500

    Bootstrap toast to academy cinemas

--- projects/AcademyCinemas/javascript/javascript.js
+++ projects/AcademyCinemas/javascript/javascript.js
@@ -11,3 +11,27 @@ popoverTriggerList.forEach(function (element) {
         trigger: 'hover',
     });
 });
+
+//initialize bootstrap Toast
+const toastElList = document.querySelectorAll('.toast')
+const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl))
+
+//Function to display toast with selected options
+function displaySelectedMovieOptions() {
+    var movie = document.getElementById('movieSelect').options[document.getElementById('movieSelect').selectedIndex].text;
+    var time = document.getElementById('timeSelect').options[document.getElementById('timeSelect').selectedIndex].text;
+    var quantity = document.getElementById('quantity').value;
+
+    var message = "Purchase confirmed for: " + movie + "\nTime: " + time + "\nTickets: " + quantity;
+
+    //Display toast
+    var toastBody = document.getElementById('toastBody');
+    toastBody.textContent = message;
+    var toast = new bootstrap.Toast(document.getElementById('toastDisplay'));
+    toast.show()
+}
+
+
+function buyTickets() {
+    displaySelectedMovieOptions();
+}
