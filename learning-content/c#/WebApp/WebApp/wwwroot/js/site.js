a62ebbcf8177e7250b76e71c82d4f4e3f29b9ec6
MYOR-Mitchell <Martin.Mitchell@MyYahoo.com>
2025-01-10 12:31:13 -0500

    Content and organization.

---
+++ learning-content/c#/WebApp/WebApp/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function newColor() {
+    document.getElementById("practice").style.color = "blue";
+}
+
+function oldColor() {
+    document.getElementById("practice").style.color = "darkolivegreen";
+}
