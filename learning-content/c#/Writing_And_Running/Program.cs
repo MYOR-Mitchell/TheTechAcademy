7c77b2c2d795a9203fd572063c3eaffde280903b
MYOR-Mitchell <Martin.Mitchell@MyYahoo.com>
2025-01-14 08:02:50 -0500

    Update Program.cs

--- learning-content/c#/Writing_And_Running/Program.cs
+++ learning-content/c#/Writing_And_Running/Program.cs
@@ -4,8 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to Acme Accounting Systems");
-        Console.WriteLine(@"Remember, we're ""accounting"" on you!");  //OR this way... Console.WriteLine("Remember, we're \"accounting\" on you!");
-        Console.ReadKey();
+        //Console.WriteLine("Welcome to Acme Accounting Systems");
+        //Console.WriteLine(@"Remember, we're ""accounting"" on you!");  //OR this way... Console.WriteLine("Remember, we're \"accounting\" on you!");
+        //Console.ReadKey();
+
+        Console.WriteLine("What is your favorite number?");
+        string favoriteNumber = Console.ReadLine();
+        int favNum = Convert.ToInt32(favoriteNumber);
+        int total = favNum + 5;
+        Console.WriteLine("Your favorite number plus 5 is: " + total);
+        Console.ReadLine();
     }
 }
