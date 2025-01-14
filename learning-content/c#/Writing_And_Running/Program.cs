1b6f189d2b2e42361d4501114553470f720dee10
MYOR-Mitchell <Martin.Mitchell@MyYahoo.com>
2025-01-14 07:53:43 -0500

    .
    .

--- learning-content/c#/Writing_And_Running/Program.cs
+++ learning-content/c#/Writing_And_Running/Program.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine(@"Remember, we're ""accounting"" on you!");  //OR this way... Console.WriteLine("Remember, we're \"accounting\" on you!");
         Console.ReadKey();
     }
 }
