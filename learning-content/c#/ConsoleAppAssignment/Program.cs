764bf5c5c9887d304327850e3f4a539d31c9785f
MYOR-Mitchell <Martin.Mitchell@MyYahoo.com>
2025-01-20 09:14:40 -0500

    app assignment update

---
+++ learning-content/c#/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray = { "This", "is", "my", "array" };
+
+            Console.WriteLine("Enter some text, please: ");
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                stringArray[i] += " " + input;
+            }
+
+            for (int j = 0; j < stringArray.Length; j++)
+            {
+                Console.WriteLine(stringArray[j]);
+            }
+
+
+            //This is an infinite loop.
+            //while (true)  
+            //{
+            //    if (stringArray.Length > 10)
+            //    {
+            //        Console.WriteLine("Theres 10 elements in that array.");
+            //    }
+            //    else if (stringArray.Length < 10)
+            //    {
+            //        Console.WriteLine("There's not 10 elements in that array.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("How'd I get here?");
+            //    }
+            //}
+
+            //This is an infinite loop fixed.  It needed an exit to the loop. 
+            while (true)
+            {
+                if (stringArray.Length > 10)
+                {
+                    Console.WriteLine("Theres 10 elements in that array.");
+                    break;
+                }
+                else if (stringArray.Length <= 10)
+                {
+                    Console.WriteLine("There's not 10 elements in that array.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("How'd I get here?");
+                    break;
+                }
+            }
+
+
+            //Section two of the assignment assignment
+            List<string> identical = new List<string> { "One", "Two", "Three", "Three", "Four", "Five" };
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ learning-content/c#/String_Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System; // Required for basic console and system operations
+using System.Text; // Required for StringBuilder
+
+namespace String_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string start = "You are ";
+            string middle = "the middle ";
+            string last = "if the sentence!";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(start);
+            sb.Append(middle.ToUpper());
+            sb.Append(last);
+            Console.WriteLine(sb.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
